44dedb36acf6f17352a73d328bc523b7f1794163
ifuryst <ifuryst@gmail.com>
2025-02-28 17:20:33 +0800

    feat(server): Add WeChat OAuth login support.
    Signed-off-by: ifuryst <ifuryst@gmail.com>

--- internal/helper/auth/oauth.go
+++ internal/helper/auth/oauth.go
@@ -65,3 +65,40 @@ func Google(ctx context.Context, clientId, clientSecret, redirectURL, code strin
 	}
 	return data, nil
 }
+
+// WeChat get user info from wechat
+//
+// Official doc:
+//
+// https://developers.weixin.qq.com/doc/oplatform/Mobile_App/WeChat_Login/Development_Guide.html
+//
+// https://developers.weixin.qq.com/doc/oplatform/Mobile_App/WeChat_Login/Authorized_API_call_UnionID.html
+func WeChat(ctx context.Context, clientId, clientSecret, redirectURL, code string) (map[string]interface{}, error) {
+	config := oauth2.Config{
+		ClientID:     clientId,
+		ClientSecret: clientSecret,
+		RedirectURL:  redirectURL,
+		Endpoint: oauth2.Endpoint{
+			TokenURL: "https://api.weixin.qq.com/sns/oauth2/access_token",
+		},
+	}
+
+	token, err := config.Exchange(ctx, code)
+	if err != nil {
+		return nil, err
+	}
+
+	client := config.Client(ctx, token)
+	resp, err := client.Get("https://api.weixin.qq.com/sns/userinfo")
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	var data map[string]interface{}
+	err = sonic.ConfigDefault.NewDecoder(resp.Body).Decode(&data)
+	if err != nil {
+		return nil, err
+	}
+	return data, nil
+}
