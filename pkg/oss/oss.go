4a94fd06ec9b55d92e59865067996e7258da8921
aide <1058165620@qq.com>
2025-03-27 17:33:39 +0800

    feat(oss): add Aliyun OSS support
    - Implement Aliyun OSS client for file management
    - Add support for multipart uploads and public URL generation
    - Include unit tests for Aliyun OSS functionality
    - Update dependencies to include Aliyun OSS SDK

---
+++ pkg/oss/oss.go
@@ -0,0 +1,49 @@
+package oss
+
+import (
+	"time"
+)
+
+type InitiateMultipartUploadResult struct {
+	UploadID   string `json:"uploadId"`
+	BucketName string `json:"bucketName"`
+	ObjectKey  string `json:"objectKey"`
+}
+
+type UploadPartInfo struct {
+	UploadID       string `json:"uploadId"`
+	BucketName     string `json:"bucketName"`
+	ObjectKey      string `json:"objectKey"`
+	PartNumber     int    `json:"partNumber"`
+	UploadURL      string `json:"uploadUrl"`
+	ExpirationTime int64  `json:"expirationTime"`
+}
+
+type CompleteMultipartUploadResult struct {
+	Location   string `json:"location"`
+	Bucket     string `json:"bucket"`
+	Key        string `json:"key"`
+	ETag       string `json:"eTag"`
+	PrivateURL string `json:"privateURL"`
+	PublicURL  string `json:"publicURL"`
+	Expiration int64  `json:"expiration"`
+}
+
+type UploadPart struct {
+	PartNumber int    `json:"partNumber"` // Part number
+	ETag       string `json:"eTag"`       // ETag value of the part's data
+}
+
+type FileManager interface {
+	InitiateMultipartUpload(objectKey string) (*InitiateMultipartUploadResult, error)
+	GenerateUploadPartURL(uploadID, objectKey string, partNumber int, expires time.Duration) (*UploadPartInfo, error)
+	CompleteMultipartUpload(uploadID, objectKey string, parts []UploadPart) (*CompleteMultipartUploadResult, error)
+	GeneratePublicURL(objectKey string, exp time.Duration) (string, error)
+}
+
+type Config interface {
+	GetEndpoint() string
+	GetAccessKeyID() string
+	GetAccessKeySecret() string
+	GetBucketName() string
+}
